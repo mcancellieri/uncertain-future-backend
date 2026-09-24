adac13cf224e3b85429083278979eec3ed894945
Matteo Cancellieri <matteo.cancellieri@open.ac.uk>
2026-09-24 14:14:04 +0100

    fix path

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 # Base directory paths
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ASSETS_DIR = os.path.join(BASE_DIR, "assets")
+ASSETS_DIR = os.path.join(BASE_DIR, "assets", "uncertainfutures")
 
 print("Preloading audio assets into RAM...")
 
